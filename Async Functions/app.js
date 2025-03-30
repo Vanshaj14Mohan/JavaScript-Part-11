e7ba905788644d0b4b0352a3a235fc5f6d69168f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-30 23:52:20 +0530

    first commit

---
+++ Async Functions/app.js
@@ -0,0 +1,7 @@
+//async and await keyword
+//async keyword is used to declare a function as asynchronous.It is used to define a function that can be paused create an async function
+//and by default all these async function return a promise
+
+async function greet(){ //it will return a promise even if we don't return anything as it is a async function
+    return "Hello"; //returns a promise
+}
