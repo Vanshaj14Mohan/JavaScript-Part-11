574b68616a12660b7e7d86e2c5e2a1d4e1a8802e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-31 01:00:50 +0530

    Await Keyword

---
+++ Async Functions/Await keyword/app.js
@@ -0,0 +1,39 @@
+//Await keyword pauses the execution of it;s surrounding async function until the promise is settled (resolved or rejected).
+function getNum(){
+    return new Promise((resolve, reject) => {
+       setTimeout(()=>{
+        let num = Math.floor(Math.random() * 10)+ 1;
+        console.log(num);
+        resolve();
+       }, 1000);
+    });
+}
+
+async function demo(){
+    await getNum();
+    await getNum();
+    getNum();
+}
+
+//Note: We can only use await keyword only in our async function. If we use it in a normal function, it will throw an error.
+
+h2 = document.querySelector("h2");
+
+function changeColor(color, delay){ //nextColChange is basically a callback here.
+    return new Promise((resolve, reject)=>{
+        setTimeout(() =>{
+            h2.style.color = color;
+            resolve("Color Changed");
+        }, delay);
+    });
+}
+
+async function color(){
+    await changeColor("red", 1000);
+    await changeColor("purple", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("green", 1000);
+    await changeColor("orange", 1000);
+}
+
+color();
