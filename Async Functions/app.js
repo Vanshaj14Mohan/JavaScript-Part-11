7ea15ff3645020f8f88117f508e090ed2a654fb0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-31 00:12:42 +0530

    async functions

--- Async Functions/app.js
+++ Async Functions/app.js
@@ -4,14 +4,22 @@
 
 async function greet(){ //it will return a promise even if we don't return anything as it is a async function
     //abc.abc(); //it will give error
-    throw "Error found" //used throw keyword to throw an error
-    return "Hello"; //returns a promise
+    //throw "404 page not found" //used throw keyword to throw an error
+    return "Hello"; //returning a value from a async function will return a promise
 }
 
 greet()
-.then(()=>{
+.then((result)=>{
     console.log("Promised was resolved");
+    console.log("Result was: ", result);
 })
-.catch(()=>{
-    console.log("Promise was rejected with error", error);//
+.catch((err)=>{
+    console.log("Promise was rejected with error: ", err);
 });
+
+//We can even make our arrow functions async too
+let hello = async ()=>{
+    return 7;
+}
+
+//hello(), in console and it would give us promise fulfilled with promise result is 7
