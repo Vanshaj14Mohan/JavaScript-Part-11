819fce173a780389d4abd0bf186bef4a4e69413f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-30 23:59:12 +0530

    files

--- Async Functions/app.js
+++ Async Functions/app.js
@@ -3,5 +3,15 @@
 //and by default all these async function return a promise
 
 async function greet(){ //it will return a promise even if we don't return anything as it is a async function
+    //abc.abc(); //it will give error
+    throw "Error found" //used throw keyword to throw an error
     return "Hello"; //returns a promise
 }
+
+greet()
+.then(()=>{
+    console.log("Promised was resolved");
+})
+.catch(()=>{
+    console.log("Promise was rejected with error", error);
+});
