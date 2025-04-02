0264562b6e67ca2b4a0aecea071b7f25498b9733
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-02 20:03:16 +0530

    API requests

--- API request/Async-await/app.js
+++ API request/Async-await/app.js
@@ -9,8 +9,9 @@ async function getCatFact(){
     let data = await res.json(); //It's also an asynchronous function it also returns a promise.
     console.log(data.fact);
 
-    let res2 = await fetch(url);
-    let data2 = await res2.json(); //It's also an asynchronous function it also returns a promise.
+    //Doing Chaining now
+    let res2 = await fetch(url); //We can call fetch function multiple times.
+    let data2 = await res2.json(); 
     console.log(data2.fact);
     }
     catch(err){
